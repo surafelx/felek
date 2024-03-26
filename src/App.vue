2ecbb4757723d35397cbad965735d722e9f5914e
luybrand <surafelyimam.sy@gmail.com>
2024-03-26 16:34:40 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <div class="background-section">
+    <Navbar />
+    <div class="hero-container">
+      <img class="audio-wave" src="/audio.gif" />
+      <img id="hero-image" src="/felek-icon.svg" />
+      <img class="audio-wave second" src="/audio.gif" />
+    </div>
+    <div class="social-bar-container">
+     <div class="social-header">
+      <h2>Join Us</h2>
+     </div>
+    </div>
+
+  </div>
+
+
+</template>
+
+<style scoped>
+
+h2 {
+  color: white;
+  font-family: 'Lemon Milk';
+}
+.hero-container {
+  display: flex;
+  justify-content: space-evenly;
+  height: 80vh;
+  align-items: center;
+}
+
+.social-bar-container {
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+}
+
+.audio-wave {
+  width: 400px;
+  height: 400px;
+  object-fit: cover;
+  object-position: 25% 25%;
+}
+
+.second {
+  transform: scaleX(-1);
+}
+
+#hero-image {
+  height: 60vh;
+  display: block;
+  filter: grayscale(100%);
+  mix-blend-mode: difference;
+}
+
+.background-section {
+  position: fixed;
+  background-image: url('./assets/background.jpg');
+  background-size: cover;
+  background-position: top;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+    <nav class="navbar">
+        <div class="container flex justify-evenly">
+            <ul class="navbar-nav flex">
+                <li class="nav-item">
+                    <a href="#about">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#services">Packs</a>
+                </li>
+            </ul>
+            <a class="navbar-brand" href="#"> <img src="/Felek.svg" class="logo" alt="Felek logo" /></a>
+            <ul class="navbar-nav flex">
+                <li class="nav-item">
+                    <a href="#about">Events</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#services">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+.logo {
+    fill: white;
+    height: 200px;
+    margin-top: 40px;
+}
+
+.navbar {
+    font-family: 'Lemon Milk';
+    height: 100px;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 10px 0;
+    z-index: 1000;
+    /* Ensure it's above other content */
+}
+
+.container {
+    display: flex;
+    justify-content: space-between;
+    height: 100px;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.navbar-nav {
+    gap: 40px;
+    padding: 0px 30px;
+}
+
+.navbar-brand,
+.nav-item {
+    color: #fff;
+    text-decoration: none;
+    padding: 0 10px;
+}
+
+.nav-item a {
+    text-decoration: none;
+    color: #fff;
+}
+
+.nav-item a:hover {
+    color: #ccc;
+}
+</style>
